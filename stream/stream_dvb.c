b2818350dd17a1506a949ec5deaefc2ce644ace9
0p1pp1 <0p1pp1@fms.freenet>
2017-06-07 01:53:34 +0900

    fix track selection from ISDB-T/S DVB input
    The new channels.conf format for ISDB-T/S only specifies program id
    of each channel, instead of specifying every PES pids.
    Since the PES streams of one channel are not filtered at DVB device,
    they must be filtered/selected at the demuxer with the channel's program id.

--- stream/stream_dvb.c
+++ stream/stream_dvb.c
@@ -873,6 +873,9 @@ static int dvbin_stream_control(struct stream *s, int cmd, void *arg)
     dvb_channels_list_t *list = state->adapters[state->cur_adapter].list;
 
     switch (cmd) {
+    case STREAM_CTRL_DVB_GET_SID:
+        (*(unsigned int*)arg) = list->channels[list->current].service_id;
+        return STREAM_OK;
     case STREAM_CTRL_GET_METADATA: {
         struct mp_tags *metadata = talloc_zero(NULL, struct mp_tags);
         char *progname = list->channels[list->current].name;
