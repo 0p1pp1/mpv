2592f36ec4063ba91fd849bc55fdd70ccab9c6ee
0p1pp1 <0p1pp1@users.noreply.github.com>
2019-11-29 02:32:18 +0900

    restore program switch feature and improve it
    - restore once removed 'program' property
    - takes user-specified language/pid options into account
    when selecting elemenary streams.

--- player/command.c
+++ player/command.c
@@ -2113,6 +2113,53 @@ static int property_current_tracks(void *ctx, struct m_property *prop,
     return mp_property_do(name, ka->action, ka->arg, ctx);
 }
 
+static int mp_property_program(void *ctx, struct m_property *prop,
+                               int action, void *arg)
+{
+    MPContext *mpctx = ctx;
+    demux_program_t prog = {0};
+
+    struct demuxer *demuxer = mpctx->demuxer;
+    if (!demuxer || !mpctx->playback_initialized)
+        return M_PROPERTY_UNAVAILABLE;
+
+    switch (action) {
+    case M_PROPERTY_SWITCH:
+    case M_PROPERTY_SET:
+        if (action == M_PROPERTY_SET && arg)
+            prog.progid = *((int *) arg);
+        else
+            prog.progid = -1;
+        fill_demux_prog_arg(mpctx, &prog);
+        if (!demuxer->desc->identify_program ||
+            !demuxer->desc->identify_program(demuxer, &prog))
+            return M_PROPERTY_ERROR;
+
+        if (prog.dmxid[STREAM_VIDEO] < 0 && prog.dmxid[STREAM_AUDIO] < 0) {
+            MP_ERR(mpctx, "Selected program contains no audio or video streams!\n");
+            return M_PROPERTY_ERROR;
+        }
+        MP_INFO(mpctx, "selected program:%d.\n", prog.progid);
+        mp_switch_track(mpctx, STREAM_VIDEO,
+          mp_track_by_dmxid(mpctx, STREAM_VIDEO, prog.dmxid[STREAM_VIDEO]), 0);
+        mp_switch_track(mpctx, STREAM_AUDIO,
+          mp_track_by_dmxid(mpctx, STREAM_AUDIO, prog.dmxid[STREAM_AUDIO]), 0);
+        mp_switch_track(mpctx, STREAM_SUB,
+            mp_track_by_dmxid(mpctx, STREAM_SUB, prog.dmxid[STREAM_SUB]), 0);
+        print_track_list(mpctx, "Program switched:");
+        reset_playback_state(mpctx);
+        return M_PROPERTY_OK;
+    case M_PROPERTY_GET_TYPE:
+        *(struct m_option *)arg = (struct m_option){
+            .type = CONF_TYPE_INT,
+            .flags = M_OPT_TYPE_USES_RANGE,
+            M_RANGE(-1, (1 <<16) - 1),
+        };
+        return M_PROPERTY_OK;
+    }
+    return M_PROPERTY_NOT_IMPLEMENTED;
+}
+
 static int mp_property_hwdec_current(void *ctx, struct m_property *prop,
                                      int action, void *arg)
 {
@@ -3628,6 +3675,7 @@ static const struct m_property mp_properties_base[] = {
     {"partially-seekable", mp_property_partially_seekable},
     {"idle-active", mp_property_idle},
     {"window-id", mp_property_window_id},
+    {"program", mp_property_program},
 
     {"chapter-list", mp_property_list_chapters},
     {"track-list", property_list_tracks},
